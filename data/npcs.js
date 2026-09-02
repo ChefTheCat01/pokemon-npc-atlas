27ca2b155e9da3b2877f35d8cc9a4977015b2740
Two of Roses <102992132+ChefTheCat01@users.noreply.github.com>
2026-09-02 12:47:34 +0200

    Create NPC data file

---
+++ data/npcs.js
@@ -0,0 +1,12 @@
+const npcData = [
+
+    {
+        name: "Professor Oak",
+        region: "Kanto",
+        location: "Pallet Town",
+        x: 509,
+        y: 473,
+        status: "Available"
+    }
+
+];
