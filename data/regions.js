ed4721e4dd50734b751ad8987cc1d5c01a6a8075
Two of Roses <102992132+ChefTheCat01@users.noreply.github.com>
2026-09-03 13:35:46 +0200

    Fix formatting and remove unnecessary lines in regions.js

--- data/regions.js
+++ data/regions.js
@@ -1,53 +1,31 @@
 const regionData = {
 
     Kanto: {
-
         name: "Kanto",
-
         image: "maps/kanto.png",
-
         width: 2048,
-
         height: 1448
-
     },
-
 
     Johto: {
-
         name: "Johto",
-
         image: "maps/johto.png",
-
         width: 1961,
-
         height: 1316
-
     },
 
-
     Hoenn: {
-
         name: "Hoenn",
-
         image: "maps/hoenn.png",
-
         width: 1250,
-
         height: 884
-
     },
 
     Sinnoh: {
-
         name: "Sinnoh",
-
         image: "maps/sinnoh.png",
-
         width: 1024,
-
         height: 724
-
-}
+    }
 
 };
