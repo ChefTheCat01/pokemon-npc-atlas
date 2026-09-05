cc0520250513cda666393e02b3a4156156a18cf8
Two of Roses <102992132+ChefTheCat01@users.noreply.github.com>
2026-09-05 12:21:55 +0200

    Remove unused trainer sprite entries
    Removed multiple trainer sprite entries from the trainerSprites object.

--- data/trainer-sprites.js
+++ data/trainer-sprites.js
@@ -2,23 +2,4 @@ const trainerSprites = {
 
     'Gym Trainer':
         'sprites/trainers/gym-trainer.png'
-
-        'Hiker m':
-        'sprites/trainers/hiker-m.png',
-
-    'Hiker f':
-        'sprites/trainers/hiker-f.png',
-
-    'Swimmer m':
-        'sprites/trainers/swimmer-m.png',
-
-    'Swimmer f':
-        'sprites/trainers/swimmer-f.png',
-
-    'Lass':
-        'sprites/trainers/lass.png',
-
-    'Youngster':
-        'sprites/trainers/youngster.png'
-
 };
