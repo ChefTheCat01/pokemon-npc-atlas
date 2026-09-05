46037afd4d63fe76d342ca44e25223b21bea4966
Two of Roses <102992132+ChefTheCat01@users.noreply.github.com>
2026-09-05 12:31:31 +0200

    Add trainer sprites

--- data/trainer-sprites.js
+++ data/trainer-sprites.js
@@ -1,3 +1,18 @@
 const trainerSprites = {
 
+    'Gym Trainer':
+        'sprites/trainers/gym-trainer.png',
+
+    'Swimmer':
+        'sprites/trainers/swimmer.png',
+
+    'Swimmer 2':
+        'sprites/trainers/swimmer-2.png',
+
+    'Swimmer f':
+        'sprites/trainers/swimmer-f.png',
+
+    'Swimmer 2f':
+        'sprites/trainers/swimmer-2f.png'
+
 };
