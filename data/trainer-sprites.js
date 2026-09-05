a29cdced79b89713bf7ae8c2ef67f5455225414f
Two of Roses <102992132+ChefTheCat01@users.noreply.github.com>
2026-09-05 15:19:40 +0200

    Update trainer sprites with new entries
    Added new trainer sprites for various characters.

--- data/trainer-sprites.js
+++ data/trainer-sprites.js
@@ -1,18 +1,295 @@
 const trainerSprites = {
 
-    'Gym Trainer':
-        'sprites/trainers/gym-trainer.png',
+    'Ace Trainer f': 'sprites/trainers/acetrainer-f.png',
+    'Ace Trainer': 'sprites/trainers/acetrainer.png',
+    'Ace Trainer Couple': 'sprites/trainers/acetrainercouple.png',
+    'Ace Trainer Snow f': 'sprites/trainers/acetrainersnow-f.png',
+    'Ace Trainer Snow': 'sprites/trainers/acetrainersnow.png',
 
-    'Swimmer':
-        'sprites/trainers/swimmer.png',
+    'Aether Employee f': 'sprites/trainers/aetheremployee-f.png',
+    'Aether Employee': 'sprites/trainers/aetheremployee.png',
 
-    'Swimmer 2':
-        'sprites/trainers/swimmer-2.png',
+    'Aether Foundation 2': 'sprites/trainers/aetherfoundation-2.png',
+    'Aether Foundation f': 'sprites/trainers/aetherfoundation-f.png',
+    'Aether Foundation': 'sprites/trainers/aetherfoundation.png',
 
-    'Swimmer f':
-        'sprites/trainers/swimmer-f.png',
+    'Aqua Grunt f': 'sprites/trainers/aquagrunt-f.png',
+    'Aqua Grunt': 'sprites/trainers/aquagrunt.png',
 
-    'Swimmer 2f':
-        'sprites/trainers/swimmer-2f.png'
+    'Aroma Lady': 'sprites/trainers/aromalady.png',
+
+    'Artist f': 'sprites/trainers/artist-f.png',
+    'Artist': 'sprites/trainers/artist.png',
+
+    'Backers f': 'sprites/trainers/backers-f.png',
+    'Backers': 'sprites/trainers/backers.png',
+
+    'Backpacker f': 'sprites/trainers/backpacker-f.png',
+    'Backpacker': 'sprites/trainers/backpacker.png',
+
+    'Baker': 'sprites/trainers/baker.png',
+    'Ball Guy': 'sprites/trainers/ballguy.png',
+    'Battle Girl': 'sprites/trainers/battlegirl.png',
+    'Beauty': 'sprites/trainers/beauty.png',
+    'Bellhop': 'sprites/trainers/bellhop.png',
+    'Biker': 'sprites/trainers/biker.png',
+    'Bird Keeper': 'sprites/trainers/birdkeeper.png',
+
+    'Black Belt 2': 'sprites/trainers/blackbelt-2.png',
+    'Black Belt': 'sprites/trainers/blackbelt.png',
+
+    'Boarder': 'sprites/trainers/boarder.png',
+
+    'Bodybuilder f': 'sprites/trainers/bodybuilder-f.png',
+    'Bodybuilder': 'sprites/trainers/bodybuilder.png',
+
+    'Bug Catcher': 'sprites/trainers/bugcatcher.png',
+    'Bug Maniac': 'sprites/trainers/bugmaniac.png',
+    'Burglar': 'sprites/trainers/burglar.png',
+    'Butler': 'sprites/trainers/butler.png',
+    'Cabbie': 'sprites/trainers/cabbie.png',
+    'Cafe Master': 'sprites/trainers/cafemaster.png',
+    'Cameraman': 'sprites/trainers/cameraman.png',
+    'Camper': 'sprites/trainers/camper.png',
+    'Caretaker': 'sprites/trainers/caretaker.png',
+    'Channeler': 'sprites/trainers/channeler.png',
+    'Chef': 'sprites/trainers/chef.png',
+
+    'Clerk f': 'sprites/trainers/clerk-f.png',
+    'Clerk': 'sprites/trainers/clerk.png',
+
+    'Collector': 'sprites/trainers/collector.png',
+    'Cook': 'sprites/trainers/cook.png',
+    'Crush Girl': 'sprites/trainers/crushgirl.png',
+    'Crush Kin': 'sprites/trainers/crushkin.png',
+    'Cue Ball': 'sprites/trainers/cueball.png',
+
+    'Cyclist f': 'sprites/trainers/cyclist-f.png',
+    'Cyclist': 'sprites/trainers/cyclist.png',
+
+    'Dancer 2': 'sprites/trainers/dancer-2.png',
+    'Dancer f': 'sprites/trainers/dancer-f.png',
+    'Dancer': 'sprites/trainers/dancer.png',
+
+    'Delinquent 2f': 'sprites/trainers/delinquent-2f.png',
+    'Delinquent f': 'sprites/trainers/delinquent-f.png',
+    'Delinquent': 'sprites/trainers/delinquent.png',
+
+    'Doctor f': 'sprites/trainers/doctor-f.png',
+    'Doctor': 'sprites/trainers/doctor.png',
+
+    'Double Team': 'sprites/trainers/doubleteam.png',
+    'Dragon Tamer': 'sprites/trainers/dragontamer.png',
+    'Engineer': 'sprites/trainers/engineer.png',
+
+    'Expert f': 'sprites/trainers/expert-f.png',
+    'Expert': 'sprites/trainers/expert.png',
+
+    'Fisherman 2': 'sprites/trainers/fisherman-2.png',
+    'Fisherman': 'sprites/trainers/fisherman.png',
+
+    'Flare Grunt f': 'sprites/trainers/flaregrunt-f.png',
+    'Flare Grunt': 'sprites/trainers/flaregrunt.png',
+
+    'Free Diver': 'sprites/trainers/freediver.png',
+
+    'Furisode Girl Black': 'sprites/trainers/furisodegirl-black.png',
+    'Furisode Girl Blue': 'sprites/trainers/furisodegirl-blue.png',
+    'Furisode Girl Pink': 'sprites/trainers/furisodegirl-pink.png',
+    'Furisode Girl White': 'sprites/trainers/furisodegirl-white.png',
+
+    'Galactic Grunt f': 'sprites/trainers/galacticgrunt-f.png',
+    'Galactic Grunt': 'sprites/trainers/galacticgrunt.png',
+
+    'Gambler': 'sprites/trainers/gambler.png',
+    'Gamer': 'sprites/trainers/gamer.png',
+    'Garcon': 'sprites/trainers/garcon.png',
+    'Gardener': 'sprites/trainers/gardener.png',
+
+    'Gentleman 2': 'sprites/trainers/gentleman-2.png',
+    'Gentleman': 'sprites/trainers/gentleman.png',
+
+    'Golfer': 'sprites/trainers/golfer.png',
+
+    'Guitarist 2': 'sprites/trainers/guitarist-2.png',
+    'Guitarist': 'sprites/trainers/guitarist.png',
+
+    'Gym Trainer': 'sprites/trainers/gym-trainer.png',
+
+    'Harlequin': 'sprites/trainers/harlequin.png',
+
+    'Hiker 2': 'sprites/trainers/hiker-2.png',
+    'Hiker': 'sprites/trainers/hiker.png',
+
+    'Hooligans': 'sprites/trainers/hooligans.png',
+    'Hoopster': 'sprites/trainers/hoopster.png',
+    'Idol': 'sprites/trainers/idol.png',
+    'Infielder': 'sprites/trainers/infielder.png',
+    'Interviewers': 'sprites/trainers/interviewers.png',
+    'Janitor': 'sprites/trainers/janitor.png',
+    'Jogger': 'sprites/trainers/jogger.png',
+    'Juggler': 'sprites/trainers/juggler.png',
+    'Kimono Girl': 'sprites/trainers/kimonogirl.png',
+    'Kindler': 'sprites/trainers/kindler.png',
+    'Kunoichi': 'sprites/trainers/kunoichi.png',
+    'Lady': 'sprites/trainers/lady.png',
+
+    'Lass 2': 'sprites/trainers/lass-2.png',
+    'Lass': 'sprites/trainers/lass.png',
+
+    'League Staff f': 'sprites/trainers/leaguestaff-f.png',
+    'League Staff': 'sprites/trainers/leaguestaff.png',
+
+    'Linebacker': 'sprites/trainers/linebacker.png',
+
+    'Madame 2': 'sprites/trainers/madame-2.png',
+    'Madame': 'sprites/trainers/madame.png',
+
+    'Magma Grunt f': 'sprites/trainers/magmagrunt-f.png',
+    'Magma Grunt': 'sprites/trainers/magmagrunt.png',
+
+    'Maid': 'sprites/trainers/maid.png',
+    'Medium': 'sprites/trainers/medium.png',
+    'Model': 'sprites/trainers/model.png',
+
+    'Musician 2': 'sprites/trainers/musician-2.png',
+    'Musician': 'sprites/trainers/musician.png',
+
+    'Ninja Boy': 'sprites/trainers/ninjaboy.png',
+    'Nurse': 'sprites/trainers/nurse.png',
+    'Nursery Aide': 'sprites/trainers/nurseryaide.png',
+
+    'Office Worker f': 'sprites/trainers/officeworker-f.png',
+    'Office Worker': 'sprites/trainers/officeworker.png',
+
+    'Old Couple': 'sprites/trainers/oldcouple.png',
+    'Owner': 'sprites/trainers/owner.png',
+    'Parasol Lady': 'sprites/trainers/parasollady.png',
+    'Picnicker': 'sprites/trainers/picnicker.png',
+    'Pilot': 'sprites/trainers/pilot.png',
+
+    'Plasma Grunt f': 'sprites/trainers/plasmagrunt-f.png',
+    'Plasma Grunt': 'sprites/trainers/plasmagrunt.png',
+
+    'Poke Fan f': 'sprites/trainers/pokefan-f.png',
+    'Poke Fan': 'sprites/trainers/pokefan.png',
+
+    'Poke Kid': 'sprites/trainers/pokekid.png',
+    'Poke Maniac': 'sprites/trainers/pokemaniac.png',
+
+    'Pokemon Breeder f': 'sprites/trainers/pokemonbreeder-f.png',
+    'Pokemon Breeder': 'sprites/trainers/pokemonbreeder.png',
+
+    'Pokemon Ranger f': 'sprites/trainers/pokemonranger-f.png',
+    'Pokemon Ranger': 'sprites/trainers/pokemonranger.png',
+
+    'Policeman': 'sprites/trainers/policeman.png',
+    'Postman': 'sprites/trainers/postman.png',
+
+    'Preschooler': 'sprites/trainers/preschooler.png',
+    'Preschooler f': 'sprites/trainers/preschooler-f.png',
+    'Preschoolers': 'sprites/trainers/preschoolers.png',
+
+    'Psychic f': 'sprites/trainers/psychic-f.png',
+    'Psychic': 'sprites/trainers/psychic.png',
+
+    'Punk Girl': 'sprites/trainers/punkgirl.png',
+    'Punk Guy': 'sprites/trainers/punkguy.png',
+    'Rail Staff': 'sprites/trainers/railstaff.png',
+
+    'Rainbow Rocket Grunt f': 'sprites/trainers/rainbowrocketgrunt-f.png',
+    'Rainbow Rocket Grunt': 'sprites/trainers/rainbowrocketgrunt.png',
+
+    'Rancher': 'sprites/trainers/rancher.png',
+    'Reporter': 'sprites/trainers/reporter.png',
+    'Rich Boy': 'sprites/trainers/richboy.png',
+
+    'Rising Star f': 'sprites/trainers/risingstar-f.png',
+    'Rising Star': 'sprites/trainers/risingstar.png',
+
+    'Rocket Grunt f': 'sprites/trainers/rocketgrunt-f.png',
+    'Rocket Grunt': 'sprites/trainers/rocketgrunt.png',
+
+    'Roller Skater f': 'sprites/trainers/rollerskater-f.png',
+    'Roller Skater': 'sprites/trainers/rollerskater.png',
+
+    'Roughneck': 'sprites/trainers/roughneck.png',
+    'Ruin Maniac': 'sprites/trainers/ruinmaniac.png',
+    'Sage': 'sprites/trainers/sage.png',
+    'Sailor': 'sprites/trainers/sailor.png',
+    'Schoolboy': 'sprites/trainers/schoolboy.png',
+    'Schoolgirl': 'sprites/trainers/schoolgirl.png',
+
+    'School Kid f': 'sprites/trainers/schoolkid-f.png',
+    'School Kid': 'sprites/trainers/schoolkid.png',
+
+    'Scientist f': 'sprites/trainers/scientist-f.png',
+    'Scientist': 'sprites/trainers/scientist.png',
+
+    'Scuba Diver': 'sprites/trainers/scubadiver.png',
+
+    'Security Corps f': 'sprites/trainers/securitycorps-f.png',
+    'Security Corps': 'sprites/trainers/securitycorps.png',
+
+    'Shadow Triad': 'sprites/trainers/shadowtriad.png',
+
+    'Sightseer f': 'sprites/trainers/sightseer-f.png',
+    'Sightseer': 'sprites/trainers/sightseer.png',
+
+    'Sis and Bro': 'sprites/trainers/sisandbro.png',
+
+    'Skier f': 'sprites/trainers/skier-f.png',
+    'Skier': 'sprites/trainers/skier.png',
+
+    'Skull Grunt f': 'sprites/trainers/skullgrunt-f.png',
+    'Skull Grunt': 'sprites/trainers/skullgrunt.png',
+
+    'Sky Trainer f': 'sprites/trainers/skytrainer-f.png',
+    'Sky Trainer': 'sprites/trainers/skytrainer.png',
+
+    'Star Grunt S f': 'sprites/trainers/stargrunt-s-f.png',
+    'Star Grunt S': 'sprites/trainers/stargrunt-s.png',
+
+    'Star Grunt V f': 'sprites/trainers/stargrunt-v-f.png',
+    'Star Grunt V': 'sprites/trainers/stargrunt-v.png',
+
+    'Street Thug': 'sprites/trainers/streetthug.png',
+    'Striker': 'sprites/trainers/striker.png',
+    'Super Nerd': 'sprites/trainers/supernerd.png',
+    'Surfer': 'sprites/trainers/surfer.png',
+
+    'Swimmer 2': 'sprites/trainers/swimmer-2.png',
+    'Swimmer 2f': 'sprites/trainers/swimmer-2f.png',
+    'Swimmer f': 'sprites/trainers/swimmer-f.png',
+    'Swimmer': 'sprites/trainers/swimmer.png',
+
+    'Teacher': 'sprites/trainers/teacher.png',
+    'Teammates': 'sprites/trainers/teammates.png',
+
+    'Tourist 2f': 'sprites/trainers/tourist-2f.png',
+    'Tourist f': 'sprites/trainers/tourist-f.png',
+    'Tourist': 'sprites/trainers/tourist.png',
+
+    'Triathlete Biker': 'sprites/trainers/triathletebiker.png',
+    'Triathlete Runner': 'sprites/trainers/triathleterunner.png',
+    'Triathlete Swimmer': 'sprites/trainers/triathleteswimmer.png',
+
+    'Tuber f': 'sprites/trainers/tuber-f.png',
+    'Tuber': 'sprites/trainers/tuber.png',
+
+    'Twins': 'sprites/trainers/twins.png',
+
+    'Veteran f': 'sprites/trainers/veteran-f.png',
+    'Veteran': 'sprites/trainers/veteran.png',
+
+    'Waiter': 'sprites/trainers/waiter.png',
+    'Waitress': 'sprites/trainers/waitress.png',
+    'Worker': 'sprites/trainers/worker.png',
+
+    'Young Athlete f': 'sprites/trainers/youngathlete-f.png',
+    'Young Athlete': 'sprites/trainers/youngathlete.png',
+
+    'Young Couple': 'sprites/trainers/youngcouple.png',
+    'Youngster': 'sprites/trainers/youngster.png'
 
 };
